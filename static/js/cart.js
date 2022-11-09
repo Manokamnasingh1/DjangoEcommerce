1d96f6525cfaef67a652d87822f62d67b0171791
Manokamna Singh <85088233+Manokamnasingh1@users.noreply.github.com>
2022-11-10 00:22:48 +0530

    Update cart.js

--- static/js/cart.js
+++ static/js/cart.js
@@ -6,7 +6,6 @@ for (i = 0; i < updateBtns.length; i++) {
 		var action = this.dataset.action
 		console.log('productId:', productId, 'Action:', action)
 		console.log('USER:', user)
-
 		if (user == 'AnonymousUser'){
 			addCookieItem(productId, action)
 		}else{
